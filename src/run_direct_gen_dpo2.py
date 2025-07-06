9bd52ec6483c61e7e20811580489ba50700bad50
talzoomanzoo <75766939+talzoomanzoo@users.noreply.github.com>
2025-07-06 15:20:00 +0900

    commit openrlhf

--- src/run_direct_gen_dpo2.py
+++ src/run_direct_gen_dpo2.py
@@ -122,12 +122,13 @@ def parse_args():
         default=False,
         help="Whether to use beam search. Defaults to False if not specified."
     )
-    # parser.add_argument(
-    #     '--step1_path',
-    #     type=str,
-    #     default='z_filtered',
-    #     help="Path to the first stage output."
-    # )
+
+    parser.add_argument(
+        '--step1_path',
+        type=str,
+        default='z_filtered',
+        help="Path to the first stage output."
+    )
 
     parser.add_argument(
         '--run_type',
@@ -159,7 +160,7 @@ async def main(args):
     sample_limit = args.sample_limit
     skip_special_tokens = args.skip_special_tokens
     use_beam_search = args.use_beam_search
-    # step1_path = args.step1_path
+    step1_path = args.step1_path
     run_type = args.run_type
     # Set default repetition_penalty if not provided
     if repetition_penalty is None:
@@ -217,12 +218,12 @@ async def main(args):
         )
 
     
-    # with open(f'./outputs/{dataset_name}.{model_short_name}.direct.step-1.{run_type}/{step1_path}.json', mode='r', encoding='utf-8') as json_file: #fix here
-    #     first_stage_output_list = json.load(json_file)
-    #     first_stage_output_list = first_stage_output_list[:]
-    with open('./notebooks/pre-filtered/0611-1.5B-step1-full-raw-10samples.json', mode='r', encoding='utf-8') as json_file:
+    with open(f'./outputs/{dataset_name}.{model_short_name}.direct.step-1.{run_type}/{step1_path}.json', mode='r', encoding='utf-8') as json_file: #fix here
         first_stage_output_list = json.load(json_file)
         first_stage_output_list = first_stage_output_list[:]
+    # with open('./notebooks/pre-filtered/0611-1.5B-step1-full-raw-10samples.json', mode='r', encoding='utf-8') as json_file:
+    #     first_stage_output_list = json.load(json_file)
+    #     first_stage_output_list = first_stage_output_list[:]
         
     def clean_text(text):
         text = re.sub(r'<｜begin▁of▁sentence｜><｜User｜>', '', text, count=1)
