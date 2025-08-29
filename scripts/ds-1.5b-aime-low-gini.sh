81cae912361434a25d0ad0de5a58e4701440f42a
talzoomanzoo <75766939+talzoomanzoo@users.noreply.github.com>
2025-08-29 17:37:51 +0000

    updated training and inference scripts for fft models

---
+++ scripts/ds-1.5b-aime-low-gini.sh
@@ -0,0 +1,6 @@
+python ../src/run_direct_gen_uid_dev.py --model_path talzoomanzoo/deepseek-1.5b-lowest-uid-gini-fft \
+   --dataset_name math500 \
+   --split test \
+   --batch_size 30 \
+   --data_limit 30 \
+   --sample_limit 1
--- scripts/ds-1.5b.sh
+++
@@ -1,6 +0,0 @@
-python ../src/run_direct_gen_uid_dev.py --model_path deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
-   --dataset_name aime \
-   --split train \
-   --batch_size 1000 \
-   --data_limit 933 \
-   --sample_limit 10
