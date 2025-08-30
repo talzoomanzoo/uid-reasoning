7d23a9e7983f32d331ae2379b3d581d67b94a571
talzoomanzoo <75766939+talzoomanzoo@users.noreply.github.com>
2025-08-30 17:29:31 +0900

    dataset regeneration larger set script

--- scripts/ds-7b.sh
+++
@@ -1,6 +0,0 @@
-python ../src/run_direct_gen_uid_dev.py --model_path deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
-   --dataset_name aime \
-   --split test \
-   --batch_size 30 \
-   --data_limit 30 \
-   --sample_limit 5
---
+++ scripts/qwen3-1.7b-dataset.sh
@@ -0,0 +1,6 @@
+python ../src/run_direct_gen_uid_dev.py --model_path Qwen/Qwen3-1.7B \
+   --dataset_name aime \
+   --split train \
+   --batch_size 933 \
+   --data_limit 933 \
+   --sample_limit 10
