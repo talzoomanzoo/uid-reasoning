21d1cc272c1d7a265adfb465052fe0a4d8e4c358
talzoomanzoo <75766939+talzoomanzoo@users.noreply.github.com>
2025-09-03 19:47:59 +0900

    notebooks for 1.5b-large-scale-all data

--- scripts/qwen3-1.7b-dataset.sh
+++ scripts/qwen3-1.7b-dataset.sh
@@ -1,6 +1,6 @@
-python ../src/run_direct_gen_uid_open_router.py --model_path qwen/qwen3-4b:free \
+python ../src/run_direct_gen_uid_open_router.py --model_path Qwen/Qwen3-1.7B \
    --dataset_name aime \
    --split train \
-   --batch_size 1 \
-   --data_limit 1 \
-   --sample_limit 1
+   --batch_size 933 \
+   --data_limit 933 \
+   --sample_limit 8
