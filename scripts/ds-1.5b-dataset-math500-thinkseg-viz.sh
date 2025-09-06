87557d430c0f6f9e53f22cbcd94fa64d6d1fa7d7
talzoomanzoo <75766939+talzoomanzoo@users.noreply.github.com>
2025-09-06 02:31:22 +0000

    viz scripts updated

--- scripts/ds-1.5b-dataset-math500-thinkseg-viz.sh
+++ scripts/ds-1.5b-dataset-math500-thinkseg-viz.sh
@@ -1,6 +1,6 @@
-python ../src/run_direct_gen_uid_dev.py --model_path deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
-   --dataset_name math500 \
-   --split test \
+python ../src/run_direct_gen_uid_dev_viz.py --model_path deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
+   --dataset_name gpqa \
+   --split main \
    --thinkseg True \
    --batch_size 1 \
    --data_limit 1 \
