e2890100d8d71d14ae7bcc611252bd3db1288552
talzoomanzoo <75766939+talzoomanzoo@users.noreply.github.com>
2025-10-15 17:56:30 +0900

    delete unnecessary files

--- scripts/list_torch_versions.py
+++
@@ -1,8 +0,0 @@
-import pkgutil, importlib, sys
-pkgs = []
-for name in ["torch","torchvision","torchaudio"]:
-    m = importlib.import_module(name)
-    import pkg_resources as pr
-    dist = pr.get_distribution(name)
-    print(dist.project_name, dist.version)
-
